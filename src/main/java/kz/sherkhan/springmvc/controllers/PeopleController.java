0321f5e73beb71e0e3faf63d977bc4ede48c59cf
Sherkhan <kur.sherkhan2000@gmail.com>
2022-08-25 12:12:30 +0600

    Lesson 22

--- src/main/java/kz/sherkhan/springmvc/controllers/PeopleController.java
+++ src/main/java/kz/sherkhan/springmvc/controllers/PeopleController.java
@@ -2,12 +2,11 @@ package kz.sherkhan.springmvc.controllers;
 
 
 import kz.sherkhan.springmvc.dao.PersonDAO;
+import kz.sherkhan.springmvc.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -33,6 +32,18 @@ public class PeopleController {
         //Получим одного человака из DAO и передадим его на отоброжение в представлений
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+
     }
 
 }
--- src/main/java/kz/sherkhan/springmvc/dao/PersonDAO.java
+++ src/main/java/kz/sherkhan/springmvc/dao/PersonDAO.java
@@ -21,13 +21,17 @@ public class PersonDAO {
 
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
+    public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 
 }
